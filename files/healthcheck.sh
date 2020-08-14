22b1da3e39410c2a434ce3d7e20c4873b63e2165
Katie Holly <git@meo.ws>
2020-08-14 13:29:42 +0200

    Revert "Remove anyqast-dnstest.org from health check"
    This reverts commit 218b0005dbddcb98190f5a9250ed95b8fda22c58.

--- files/healthcheck.sh
+++ files/healthcheck.sh
@@ -7,8 +7,7 @@ healthcheck_ip=$(cat /tmp/healthcheck-ip 2> /dev/null)
 test -z "${healthcheck_ip}" && exit 1
 
 retval=$(
-	#for domain in anyqast-dnstest.com anyqast-dnstest.org; do
-	for domain in anyqast-dnstest.com; do
+	for domain in anyqast-dnstest.com anyqast-dnstest.org; do
 		for test in 'A 0.0.0.0' 'AAAA ::' 'TXT "'"${domain}"'"' 'MX 0 '"${domain}"'.'; do
 			read qtype expected <<<$(echo "${test}")
 			result=$(dig +short +tries=1 +timeout=1 +noall +answer "@${healthcheck_ip}" -c IN -q "$(cat /dev/urandom | head -c 512 | sha512sum | head -c 16).${domain}." -t "${qtype}" 2>&1)
