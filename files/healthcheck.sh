71dc46b9a54d5ded2b14b9a5ccfb501553c8be80
Katie Holly <git@meo.ws>
2020-08-27 22:32:25 +0200

    Improve health check down reaction latency and use udp instead of http for influx line proto

--- files/healthcheck.sh
+++ files/healthcheck.sh
@@ -6,6 +6,11 @@ healthcheck_ip=$(cat /tmp/healthcheck-ip 2> /dev/null)
 
 test -z "${healthcheck_ip}" && exit 1
 
+if ! result=$(dig +short +tries=1 +timeout=1 +noall +answer "@${healthcheck_ip}" -c IN -q . -t SOA 2>&1); then
+	echo "Failed dnstest: domain=. qtype=SOA result=${result}" 1>&2
+	exit 1
+fi
+
 retval=$(
 	for domain in anyqast-dnstest.com anyqast-dnstest.org; do
 		for test in 'A 0.0.0.0' 'AAAA ::' 'TXT "'"${domain}"'"' 'MX 0 '"${domain}"'.'; do
